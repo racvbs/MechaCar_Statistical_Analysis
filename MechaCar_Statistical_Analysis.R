d62719117afd8adaeb559d4a7e2ab3f016306161
Raquel Valdez <85086918+racvbs@users.noreply.github.com>
2021-11-24 03:00:13 -0600

    Update

--- MechaCar_Statistical_Analysis.R
+++ MechaCar_Statistical_Analysis.R
@@ -4,8 +4,8 @@ library(tidyverse)
 library(validate)
 library(GGally)
 
-# Delivery 1
-# Read MechaCar_mpg.csv
+# MechaChar - (1)
+
 mechaCar_data <- read.csv('D:/Dropbox/Cursos/Bootcamp/Module 15 - Challenge/MechaCar_Statistical_Analysis/MechaCar_mpg.csv')
 
 # Head view of the data
@@ -20,38 +20,35 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCar_data)) 
 
-## It's multivaribale regression plot. Library GGally
+# It's multivaribale regression plot. Library GGally
 ggpairs(mechaCar_data) 
 
-# Delivery 2
-# Read Suspension_Coil.csv
-suspen_data <- read.csv('D:/Dropbox/Cursos/Bootcamp/Module 15 - Challenge/Suspension_Coil.csv')
+# Suspension Coil - (2)
+
+suspension_data <- read.csv('D:/Dropbox/Cursos/Bootcamp/Module 15 - Challenge/MechaCar_Statistical_Analysis/Suspension_Coil.csv')
 
 # Head view of the data
-head(suspen_data)
+head(suspension_data)
 
 # Get the columns names
-colnames(suspen_data)
+colnames(suspension_data)
 
 # Create a data frame with statics info
-total_summary <- suspen_data %>% group_by() %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+total_summary <- suspension_data %>% group_by() %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 # Create a data frame with statics info with group by
-lot_summary<- suspen_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary<- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 
-# Delivery3
+# Test t-test - (3)
 
 # One sample t-test
-t.test(x=suspen_data$PSI, mu=1500)
+t.test(x=suspension_data$PSI,mu=1500)
 
 # Different samples t-test
 
-# Lot1
-t.test(subset(suspen_data, Manufacturing_Lot=='Lot1')$PSI, mu=1500)
+t.test(subset(suspension_data, Manufacturing_Lot=='Lot1')$PSI, mu=1500)
 
-# Lot2
-t.test(subset(suspen_data, Manufacturing_Lot=='Lot2')$PSI, mu=1500)
+t.test(subset(suspension_data, Manufacturing_Lot=='Lot2')$PSI, mu=1500)
 
-# Lot3
-t.test(subset(suspen_data, Manufacturing_Lot=='Lot3')$PSI, mu=1500)
+t.test(subset(suspension_data, Manufacturing_Lot=='Lot3')$PSI, mu=1500)
